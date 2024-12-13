bf8b785375e4b694b7e9000ba2f6ce9e4140a6f5
SHarpreetSingh <harpreetismean@gmail.com>
2024-12-13 16:34:19 +0530

    modified configuration

---
+++ src/git.js
@@ -0,0 +1,24 @@
+let nums = [-1,1,2,3,1], target = 2,  count = 0
+
+var countPairs = function (nums, target) {
+    for (let index = 0; index < nums.length; index++) {
+        const element = nums[index];
+        // console.log("element", element);
+        nums.filter((v, i, a) => {
+            if ((a[i + 1]) === undefined) return false
+            // console.log(a.indexOf(v),nums.indexOf(element), "dd");
+            console.log("index < i+1", index < i + 1);
+            if (index < i + 1) {
+                // console.log("pppp=>", element == a[i + 1]);
+                // if (element != a[i + 1]) {
+                    console.log("sum", element, (a[i + 1]), element + (a[i + 1]), (element + (a[i + 1])) < target);
+                    if ((element + (a[i + 1])) < target) count++
+                // }
+            }
+        })
+    }
+    console.log("count", count);
+    return count
+};
+
+countPairs(nums, target)
